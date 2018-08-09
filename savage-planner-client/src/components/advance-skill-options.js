e5cb045fde242904397c0fefd070c344e4e857ca
dghero <devingreyhero@gmail.com>
2018-08-09 11:46:36 -0500

    Add missed file

---
+++ savage-planner-client/src/components/advance-skill-options.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const getAttrAbbrev = function(attr){
+  switch(attr){
+    case 'strength': return 'St';
+    case 'agility': return 'Ag';
+    case 'smarts': return 'Sm';
+    case 'spirit': return 'Sp';
+    case 'vigor': return 'Vi';
+    default: return '??';
+  }
+};
+
+export function AdvanceSkillOptions(props){
+  const skills = props.character.stats.initial.skills;
+  const skillKeys = Object.keys(skills);
+
+  const skillOptions = skillKeys.map(skill =>(
+    <option value={skill} key={`${props.key}-${skill}`}>
+      {skill.charAt(0).toUpperCase() + skill.substring(1)}
+      ({getAttrAbbrev(skills[skill].attr)})
+    </option>
+  ));
+
+  const noneOption = (<option value='none' key='none'>Select...</option>);
+
+  return ([noneOption, ...skillOptions]);
+}
+
+
+const mapStateToProps = state => ({
+  character: state.character
+});
+
+export default connect(mapStateToProps)(AdvanceSkillOptions);
